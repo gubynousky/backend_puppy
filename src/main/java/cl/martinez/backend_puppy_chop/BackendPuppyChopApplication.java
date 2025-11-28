6a3c2f1e9ec68969ac5cd40734d55359f0581e76
gubynousky <ru.martinezs@duocuc.cl>
2025-11-28 02:49:18 -0300

    primer commit

---
+++ src/main/java/cl/martinez/backend_puppy_chop/BackendPuppyChopApplication.java
@@ -0,0 +1,13 @@
+package cl.martinez.backend_puppy_chop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPuppyChopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPuppyChopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/martinez/backend_puppy_chop/BackendPuppyChopApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.martinez.backend_puppy_chop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPuppyChopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
